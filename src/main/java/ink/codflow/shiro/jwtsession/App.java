5ef9a841656bf4816fc2e4b29ff068449050c001
inwar <inwar@hotmail.com>
2019-03-07 00:49:38 +0800

    init

---
+++ src/main/java/ink/codflow/shiro/jwtsession/App.java
@@ -0,0 +1,13 @@
+package ink.codflow.shiro.jwtsession;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
